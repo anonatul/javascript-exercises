0bd7c7e6d822afb4d50848cabd9af4188b30e580
Atul <atul.kevat28@gmail.com>
2024-09-24 21:16:38 +0530

    Fix reverseString function logic and ensure proper test coverage.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+    let reversedString = "";
+    const lengthOfString = (string.length - 1);
+
+    for(let i=lengthOfString;i>=0;i--){
+        reversedString += string[i];
+    }
+    
+    return reversedString; 
 };
 
 // Do not edit below this line
