e8a8ee680ab1d2b1f759e9175e233c96d6fea0f0
Atul <atul.kevat28@gmail.com>
2024-09-25 14:29:03 +0530

    Implemented sumAll function with input validation and edge case handling

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(startNum,endNum) {
+    if(!Number.isInteger(startNum) || !Number.isInteger(endNum) || startNum < 0 || endNum < 0) return 'ERROR';
+    if(startNum > endNum){
+        let temporaryNum = startNum;
+        startNum = endNum;
+        endNum = temporaryNum;
+    }
+    let sumNum = 0;
+        for(let i=startNum;i<=endNum;i++){
+            sumNum += i;
+        }
+    return sumNum;
 };
 
 // Do not edit below this line
